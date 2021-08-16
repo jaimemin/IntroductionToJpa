e5a11e7e6fbbd9d24e4713f6bf98e91e75df4ccc
jaimemin <jaimemin@gmail.com>
2021-08-16 23:43:05 +0900

    Initial commit

---
+++ src/main/java/com/tistory/jaimemin/jpa/JpaMain.java
@@ -0,0 +1,16 @@
+package com.tistory.jaimemin.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory
+                = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
