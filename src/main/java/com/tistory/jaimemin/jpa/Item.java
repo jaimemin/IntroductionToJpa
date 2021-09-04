86b2bcfa8c7e3cae1d651f7b15f7404f4d8e1e6e
jaimemin <jaimemin@gmail.com>
2021-09-05 01:57:55 +0900

    싱글 테이블 전략

--- src/main/java/com/tistory/jaimemin/jpa/Item.java
+++ src/main/java/com/tistory/jaimemin/jpa/Item.java
@@ -2,9 +2,24 @@ package com.tistory.jaimemin.jpa;
 
 import javax.persistence.*;
 
+/**
+ * 단일 테이블 전략
+ *     create table Item (
+ *        DTYPE varchar(31) not null,
+ *         id bigint not null,
+ *         name varchar(255),
+ *         price integer not null,
+ *         actor varchar(255),
+ *         director varchar(255),
+ *         artist varchar(255),
+ *         author varchar(255),
+ *         isbn varchar(255),
+ *         primary key (id)
+ *     )
+ */
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn // DTYPE이 생기고 Entity명이 들어감
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn // 단일 테이블 전략의 경우 해당 어노테이션 없어도 생성됨
 public class Item {
 
     @Id
