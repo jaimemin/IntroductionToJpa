0b1aa082ffa508beef891805320b6f2daa1d7a23
jaimemin <jaimemin@gmail.com>
2021-09-13 02:02:59 +0900

    영속 상태일 때, reference와 엔티티 비교

--- src/main/java/com/tistory/jaimemin/jpa/JpaMain.java
+++ src/main/java/com/tistory/jaimemin/jpa/JpaMain.java
@@ -21,28 +21,19 @@ public class JpaMain {
             member.setUsername("hello");
             entityManager.persist(member);
 
-            Member member2 = new Member();
-            member.setUsername("hello2");
-            entityManager.persist(member2);
-
             entityManager.flush();
             entityManager.clear();
-
-//            Member foundMember = entityManager.find(Member.class, member.getId());
-//            Member foundMember2 = entityManager.find(Member.class, member2.getId());
-//
-//            // true
-//            System.out.println("member == member2" + (member.getClass() == member2.getClass()));
-
+                
             Member foundMember = entityManager.find(Member.class, member.getId());
-            Member foundMember2 = entityManager.getReference(Member.class, member2.getId());
-
-            // false (프록시가 넘어오는 경우 있으므로 == 사용하지 말기)
-            System.out.println("member == member2" + (member.getClass() == member2.getClass()));
-
-            // true (상속관계이므로)
-            System.out.println("member == member2: " + (member instanceof Member));
-            System.out.println("member == member2: " + (member2 instanceof Member));
+            System.out.println("foundMember.getClass() = " + foundMember.getClass());
+            
+            // 현재 영속성 컨텍스트에 foundMember가 올라와있는 상태
+            
+            Member reference = entityManager.getReference(Member.class, member.getId());
+            System.out.println("reference.getClass() = " + reference.getClass());
+            
+            // JPA에서는 같은 트랜잭션 내 즉, 같은 영속성 컨텍스트 내에서는 ==는 동일
+            System.out.println("a == a: " + (member == reference));
 
             transaction.commit();
         } catch (Exception e) {
