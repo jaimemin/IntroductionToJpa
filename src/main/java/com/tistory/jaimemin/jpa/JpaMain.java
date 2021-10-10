b1a4d82180ec2afcd3df0a8ea6b1c9f94dfd83bb
jaimemin <jaimemin@gmail.com>
2021-10-10 23:00:10 +0900

    CriteriaBuilder (실무에서는 안 쓰임)

--- src/main/java/com/tistory/jaimemin/jpa/JpaMain.java
+++ src/main/java/com/tistory/jaimemin/jpa/JpaMain.java
@@ -3,6 +3,9 @@ package com.tistory.jaimemin.jpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -18,10 +21,23 @@ public class JpaMain {
         transaction.begin();
 
         try {
-            List<Member> members = entityManager.createQuery(
-                    "SELECT m FROM Member m WHERE m.username LIKE '%kim%'",
-                    Member.class
-            ).getResultList();
+            // Criteria 사용 준비
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Member> query = criteriaBuilder.createQuery(Member.class);
+
+            Root<Member> member = query.from(Member.class);
+            CriteriaQuery<Member> criteriaQuery = query.select(member);
+
+            String username = "kim";
+
+            // 장점: 컴파일 시점에 에러 잡아줌
+            // 단점: sql 스럽지 않음
+            if (username != null) {
+                criteriaQuery = criteriaQuery.where(criteriaBuilder.equal(member.get("username"), "kim"));
+            }
+
+            List<Member> members = entityManager.createQuery(criteriaQuery)
+                            .getResultList();
 
             transaction.commit();
         } catch (Exception e) {
